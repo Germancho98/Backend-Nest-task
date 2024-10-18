4fe258bfc7d792694d93c7902ffb36c0884cf2b2
GermanSaavedra <saavedraorozcog@gmail.com>
2024-10-18 18:28:06 -0500

    feat: task project - nestJS

---
+++ src/task/task.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Post, Get, Put, Param, Delete } from '@nestjs/common';
+import { TaskService } from './task.service';
+import { TaskDTO } from './dto/task.dto'; 
+import { ITask } from 'src/task.interface';
+
+@Controller('/api/task')
+export class TaskController {
+    constructor (private readonly taskService: TaskService){}
+    @Post()
+    create(@Body() taskDTO:TaskDTO){
+        return this.taskService.create(taskDTO)
+    }
+    @Get()
+    findAll(){
+        return this.taskService.findAll()
+    }
+    @Get(":id")
+    findOne(@Param("id") id:string){
+        return this.taskService.findOne(id)
+    }
+    @Put(":id")
+    update(@Param("id") id: string, @Body() taskDTO:TaskDTO){
+        return this.taskService.update(id, taskDTO)
+    }
+    @Delete(":id")
+    delete(@Param("id") id:string){
+        return this.taskService.delete(id)
+    }
+}
